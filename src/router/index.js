86e80667ed0c6042c6f443f0228ea86f64134ef8
abdultalif <abdultalif85@gmail.com>
2024-06-07 14:29:51 +0700

    REST API fitur report

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Product from './products-routes.js'
 import User from './user-routes.js'
 import Ongkir from './ongkir-routes.js'
 import Order from './order-routes.js'
+import Report from './report-routes.js'
 
 import express from 'express';
 
@@ -15,6 +16,7 @@ router.use('/api', Product)
 router.use('/api', User)
 router.use('/api', Ongkir)
 router.use('/api', Order)
+router.use('/api', Report)
 
 router.use((req, res, next) => {
     res.status(404).json({ errors: "Periksa lagi Endpoint nya mang salahan kayanya" })
