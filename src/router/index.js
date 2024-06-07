6a8c6316c575a81064a780e2776a868f97e97042
abdultalif <abdultalif85@gmail.com>
2024-06-07 16:15:21 +0700

    REST API fitur dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import User from './user-routes.js'
 import Ongkir from './ongkir-routes.js'
 import Order from './order-routes.js'
 import Report from './report-routes.js'
+import Dashboard from './dashboard-routes.js'
 
 import express from 'express';
 
@@ -17,6 +18,7 @@ router.use('/api', User)
 router.use('/api', Ongkir)
 router.use('/api', Order)
 router.use('/api', Report)
+router.use('/api', Dashboard)
 
 router.use((req, res, next) => {
     res.status(404).json({ errors: "Periksa lagi Endpoint nya mang salahan kayanya" })
